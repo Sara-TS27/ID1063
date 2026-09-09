155756c1e9e6bc1b0155a416cea218bda1b7c5c2
Sara Tantarpale <ee26btech11227@iith.ac.in>
2026-09-09 16:37:41 +0530

    Update 5.c

--- 09-09-26/5.c
+++ 09-09-26/5.c
@@ -1,1 +1,34 @@
-404: Not Found
+//Code by Sara//
+//Date: 09-09-26//
+
+#include <stdio.h>
+#include <string.h>
+
+int palindrome(char str[])
+{
+    int i;
+    int length = strlen(str); //finds length of string//
+
+    for (i = 0; i < length / 2; i++)
+    {
+        if (str[i] != str[length - 1 - i])
+            return 0; //returns 0 if characters are not equal//
+    }
+
+    return 1; //returns 1 if string is palindrome//
+}
+
+int main()
+{
+    char str[100]; //defines string//
+
+    printf("Input: ");
+    scanf("%99s", str);
+
+    if (palindrome(str))
+        printf("Palindrome");
+    else
+        printf("Not a Palindrome");
+
+    return 0;
+}
